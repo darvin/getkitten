91efd0382ff4a46c74c7627eeb82f2c59d08a11c
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-12-09 11:26:26 -0800

    initial

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'http://rubygems.org'
 gem 'rack', '1.1'
 gem 'sinatra'
 
+gem 'httpclient'
+gem 'xmlsimple'
+gem 'rmagick'
--- app.rb
+++ app.rb
@@ -1,8 +1,47 @@
+require 'sinatra'
+require 'net/http'
+require 'httpclient'
+require 'xmlsimple'
+require 'RMagick'
+require "open-uri"
+
+
 get '/' do
-    "the time where this server lives is #{Time.now}
-    <br /><br />check out your <a href=\"/agent\">user_agent</a>"
+    "get kitten: 
+    /api/"
 end
 
-get '/agent' do
-    "you're using #{request.user_agent}"
+
+proxy = ENV['HTTP_PROXY']
+clnt = HTTPClient.new(proxy)
+# clnt.set_cookie_store("cookie.dat")
+target = ARGV.shift || "http://thecatapi.com/api/images/get"
+API_KEY = "NzQxOQ"
+
+
+get '/hi' do
+  @width = params[:width]
+  @height = params[:height]
+  xml = clnt.get(target, 
+  { 
+    "api_key" => API_KEY, 
+    "format" => "xml",
+    "results_per_page" => 1,
+    "type" => "png",
+    "size" => "small",
+  }
+  ).content
+  parsed_xml = XmlSimple.xml_in(xml)
+  image_url = parsed_xml["data"][0]["images"][0]["image"][0]["url"][0]
+  puts image_url
+  
+  
+  image = Magick::ImageList.new  
+  urlimage = open(image_url) 
+  image.from_blob(urlimage.read)
+  
+
+
+  send_file(image.path, :disposition => "inline")
+  
 end
