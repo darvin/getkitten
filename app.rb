ad8a245bf1b3416503de22f42f14c3b55fddbcc1
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-12-09 15:52:23 -0800

    index page added

--- app.rb
+++ app.rb
@@ -6,15 +6,17 @@ require "open-uri"
 require 'net/http'
 require 'cgi'
 
+set :public_folder, 'public'
+
+
 def http_get(domain,path,params)
     return Net::HTTP.get(domain, "#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))) if not params.nil?
     return Net::HTTP.get(domain, path)
 end
 
 
-get '/' do
-    "get kitten: 
-    /api/"
+get "/" do
+  redirect '/index.html'
 end
 
 
