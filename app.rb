67ec55c1ca26c58939f0aa09287db9c18150f99b
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-12-09 16:22:16 -0800

    rounded corners

--- app.rb
+++ app.rb
@@ -9,11 +9,35 @@ require 'cgi'
 set :public_folder, 'public'
 
 
+class String
+  def to_bool
+    return true if self == true || self =~ (/(true|t|yes|y|1)$/i)
+    return false if self == false || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+end
+
 def http_get(domain,path,params)
     return Net::HTTP.get(domain, "#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))) if not params.nil?
     return Net::HTTP.get(domain, path)
 end
 
+
+
+def round(source_image, radius = 10)
+
+  mask = Magick::Image.new(source_image.columns, source_image.rows) {self.background_color = 'transparent'}
+
+  # Create a white rectangle with rounded corners. This will become the
+  # mask for the area you want to retain in the original image.
+  Magick::Draw.new.stroke('none').stroke_width(0).fill('white').
+      roundrectangle(0, 0, source_image.columns, source_image.rows, radius, radius).
+      draw(mask)
+
+  # Apply the mask and write it out
+  source_image.composite!(mask, 0, 0, Magick::CopyOpacityCompositeOp)
+  source_image
+end
 
 get "/" do
   redirect '/index.html'
@@ -26,6 +50,7 @@ API_KEY = "NzQxOQ"
 get '/kitten.png' do
   @width = params[:width].to_i
   @height = params[:height].to_i
+  @round = params[:round]? params[:round].to_bool : false
   
   if (@width)
     if (@width>0&&@width<=250)
@@ -57,15 +82,18 @@ get '/kitten.png' do
   
 
   if (@width>0&&@height>0)
-    resized_image = image.resize_to_fit(@width, @height)
+    image = image.resize_to_fit(@width, @height)
   elsif (@width>0)
-    resized_image = image.resize_to_fit(@width)
-  else
-    resized_image = image
+    image = image.resize_to_fit(@width)
   end
+  
+  if (@round)
+    image = round(image)
+  end
+    
 
   content_type 'image/png'
-  resized_image.format = 'png'
-  resized_image.to_blob
+  image.format = 'png'
+  image.to_blob
   
 end
